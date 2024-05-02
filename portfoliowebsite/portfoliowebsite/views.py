571b52b1181ad779a5dbb7c0f42088223fea033f
Michael Lombardi <mikelombardi4332@gmail.com>
2024-05-01 23:47:14 -0500

    Sa16-act3

---
+++ portfoliowebsite/portfoliowebsite/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def work(request):
+    return render(request, 'work.html')
+
+def contact(request):
+    return render(request, 'contact.html')
